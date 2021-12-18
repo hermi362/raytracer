755a426f4a0c08f326a4e12fd878193cd3845a34
Herminio Gonzalez <herminio@gmail.com>
2021-12-18 15:13:05 +0900

    Add a few handy constants

--- util.h
+++ util.h
@@ -3,3 +3,5 @@
 // return true if f1 and f2 are within a certain tolerance
 bool isEqualEnough(float f1, float f2);
 
+static const float PI = 3.1415927;
+static const float ROOT2 = 1.41421356;  // sqrt(2.0)

2892a6d68df36e8873b3d4e0928a6bd401ff9b61
Herminio Gonzalez <herminio@gmail.com>
2022-03-24 09:21:22 +0900

    Mark non-modifying member functions as 'const'
    Don't know why I didn't do this from the beginning. It looks like an oversight.

--- color.h
+++ color.h
@@ -12,28 +12,28 @@ public:
   void setColor(const float& _r, const float& _g, const float& _b) {
     r = _r; g = _g; b = _b;
   }
-  std::string toString() {
+  std::string toString() const {
     const auto N = 50;
     char buffer[N];
     std::snprintf(buffer, N, "COLOR[%f, %f, %f]", r, g, b);
     return buffer;
   }
 
-  bool operator==(const Color& c) {
+  bool operator==(const Color& c) const {
     return (isEqualEnough(r, c.r) &&
             isEqualEnough(g, c.g) &&
             isEqualEnough(b, c.b) );
   }
-  Color operator+(const Color& c) {
+  Color operator+(const Color& c) const {
     return Color(r+c.r, g+c.g, b+c.b);
   }
-  Color operator-(const Color& c) {
+  Color operator-(const Color& c) const {
     return Color(r-c.r, g-c.g, b-c.b);
   }
-  Color operator*(const float a) {
+  Color operator*(const float a) const {
     return Color(r*a, g*a, b*a);
   }
-  Color operator*(const Color& c) {
+  Color operator*(const Color& c) const {
     return Color(r*c.r, g*c.g, b*c.b);
   }
 };

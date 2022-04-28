011f1d4cd788e798315cf0a2cc21de0e93af1a16
Herminio Gonzalez <herminio@gmail.com>
2022-04-28 13:41:14 +0900

    Add transformation for sphere
    This is a member variable that defines the sphere's transformation in the scene.
    
    You can think of it as the transformation that places the unit sphere at desired scene location.

--- sphere.h
+++ sphere.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include "matrix.h"
+
 struct Sphere
 {
+    // transformation to place/deform the sphere in scene
+    Matrix mTransform = id4Matrix; 
+
     Sphere() {}  // origin at (0,0,0) with unit radius
+
+    void setTransform(Matrix m) {
+        mTransform = m;
+    }
 };

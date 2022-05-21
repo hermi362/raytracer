3f6d2806bcc3d05e18538ab6b864042cb00631d8
Herminio Gonzalez <herminio@gmail.com>
2022-05-21 11:15:45 +0900

    Trivial optimization
    This used to have an explicit Vector constructor in order to
    silence compiler warning about overload ambiguity.
    Once the Tuple class was elimiated, this warning went away.

--- ray.cpp
+++ ray.cpp
@@ -2,7 +2,7 @@
 
 // Compute the position of this ray at the distance t along the ray
 Point Ray::getPosition(float t) {
-  return origin + Vector(t * direction);
+  return origin + (t * direction);
 }
 
 // return a new ray after applying transformation

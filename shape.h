25cdadaffd85404440edd79fe5ce78fef51c40a5
Herminio Gonzalez <herminio@gmail.com>
2022-06-07 12:31:36 +0900

    trivial changes
    just some extra comments

--- shape.h
+++ shape.h
@@ -9,9 +9,9 @@
 class Shape
 {
 public:
-  // transformation to place/deform the shape in scene (object2world) 
-  Matrix objectToWorld;
-  Matrix worldToObject;
+  
+  Matrix objectToWorld;  // transformation to place/deform the shape in scene 
+  Matrix worldToObject;  // the inverse transformation (cached for performance)
   Material material;
 
   Shape() : objectToWorld(id4Matrix), worldToObject(id4Matrix) {}
@@ -21,7 +21,10 @@ public:
       worldToObject = m.inverse();
   }
 
+  // calculate the normal vector at a certain suface point.
   virtual Vector getNormalAt(const Point& worldp) const = 0;
 
+  // cast a ray into shape, return true if ray intersects shape.
+  // return ray's t-value in case of intersection
   virtual bool intersect(const Ray& worldRay, float& tHit) const = 0;
 };
